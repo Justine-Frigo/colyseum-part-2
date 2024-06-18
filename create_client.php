f1781575f6154be39fdca86d06da43eaa2d19882
Justine-Frigo <justine0912@gmail.com>
2024-06-18 21:59:58 +0200

    CRUD part 2 ex 2

--- create_client.php
+++ create_client.php
@@ -1,15 +1,15 @@
-<?php 
+<?php
 
 include('./config.php');
 
-if (isset($_POST['create'])){
+if (isset($_POST['create'])) {
 
     $sql = 'INSERT INTO clients (lastName, firstName, birthDate, card, cardNumber) VALUES (:lastName, :firstName, :birthDate, :card, :cardNumber);';
 
     $card = $_POST['card'] ?? 0;
     $cardNumber = $_POST['cardNumber'];
     $birthDate = !empty($_POST['birthDate']) ? date('Y-m-d', strtotime($_POST['birthDate'])) : null;
-    
+
     $stmt = $pdo->prepare($sql);
     $stmt->bindParam(':lastName', $_POST['lastName']);
     $stmt->bindParam(':firstName', $_POST['firstName']);
@@ -17,22 +17,35 @@ if (isset($_POST['create'])){
     $stmt->bindParam(':card', $card, PDO::PARAM_INT);
     $stmt->bindParam(':cardNumber', $cardNumber, $cardNumber == '' ? PDO::PARAM_NULL : PDO::PARAM_INT);
     $stmt->execute();
-    echo 'Client créé avec succès !';
 
+    if (isset($_POST['card'])) {
+        if ($_POST['cardTypesId'] != 0) {
+            $sql = 'INSERT INTO cards (cardNumber, cardTypesId) VALUES (:cardNumber, :cardTypesId);';
+            $stmt = $pdo->prepare($sql);
+            $stmt->bindParam(':cardNumber', $cardNumber);
+            $stmt->bindParam(':cardTypesId', $_POST['cardTypesId']);
+            $stmt->execute();
+        } else {
+            echo 'Information de la carte non valide !';
+        }
+    }
+    echo 'Client créé avec succès !';
 }
 
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
+
 <body>
     <h1>Ajouter un client</h1>
-    <form action="<?php $_SERVER['PHP_SELF']?>" method="POST">
+    <form action="<?php $_SERVER['PHP_SELF'] ?>" method="POST">
         <label for="lastName">Nom</label>
         <input type="text" name="lastName"><br>
         <label for="firstName">Prénom</label>
@@ -43,9 +56,18 @@ if (isset($_POST['create'])){
         <input type="checkbox" name="card" value="1"><br>
         <label for="cardNumber">Numéro de carte (si applicable)</label>
         <input type="number" name="cardNumber"><br>
+        <label for="cardTypesId">Type de carte (si applicable)</label>
+        <select name="cardTypesId">
+            <option value="0" selected hidden>Choisissez un type de carte</option>
+            <option value="1">Fidélité</option>
+            <option value="2">Famille Nombreuse</option>
+            <option value="3">Etudiant</option>
+            <option value="4">Employé</option>
+        </select><br>
         <button type="submit" name="create">Ajouter un client</button>
-        
+
     </form>
-    
+
 </body>
+
 </html>
--- index.php
+++ index.php
@@ -1,4 +1,5 @@
 <?php 
 require 'create_client.php';
+require 'create_show.php';
 
 ?>
